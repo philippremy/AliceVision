d3b0cae6c9cb82371433a2837822c9d410ae15a8
Philipp Remy <philipp.remy@law-school.de>
2025-09-17 14:52:33 +0200

    FIX: Dependencies: Workaround in libVPX for Darwin > 24
    This commit fixes a build logic issue in libVPX, where the configure script is only able to detect Darwin up to version 24.X.X (macOS Sonoma). macOS 26 Tahoe is Darwin 25.X.X, causing the configure script to fall back to generic-gnu, which assumes a Linux host OS. As a workaround in CMake, we detect (using CMAKE_SYSTEM_VERSION) if the host Darwin is > 2. In that case, we explicitly set the newest available toolchain for libVPX, which logically is [x86_64/arm64]-darwin24-gcc.
    
    To be removed when libVPX either changes its detection logic or a newer ABI- and API-compatible version with proper Darwin > 24 support is released.
    
    Signed-off-by: Philipp Remy <philipp.remy@law-school.de>

--- src/cmake/Dependencies.cmake
+++ src/cmake/Dependencies.cmake
@@ -718,6 +718,25 @@ if(AV_BUILD_FFMPEG)
     if(AV_BUILD_VPX)
         set(VPX_TARGET libvpx)
 
+        # This is currently required until libVPX properly supports macOS 26
+        # Tahoe.
+        # The configure.sh script only detects Darwin versions up to 24.X.X,
+        # but macOS 26 Tahoe is version 25.X.X, causing the configure script
+        # to fall back to generic-gnu, which assumes Linux and therefore pulls
+        # in the wrong linker flags.
+        # If we detect macOS 26 or higher, we explicitly set the toolchain to
+        # be (x86_64/arm64)-darwin24-gcc. For this, use CMAKE_SYSTEM_VERSION.
+        set(VPX_TOOLCHAIN_FLAG)
+        if(APPLE)
+            if(CMAKE_SYSTEM_VERSION VERSION_GREATER_EQUAL 25) # Tahoe and later
+                if(CMAKE_OSX_ARCHITECTURES MATCHES "x86_64")
+                    set(VPX_TOOLCHAIN_FLAG --target=x86_64-darwin24-gcc)
+                elseif(CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
+                    set(VPX_TOOLCHAIN_FLAG --target=arm64-darwin24-gcc)
+                endif()
+            endif()
+        endif()
+
         if(APPLE AND ALICEVISION_USE_RPATH)
             set(VPX_APPLE_LDFLAGS ${CMAKE_COMMAND} -E env LDFLAGS=-Wl,-install_name,@rpath/libvpx.dylib)
         else()
@@ -735,7 +754,7 @@ if(AV_BUILD_FFMPEG)
             INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
             CONFIGURE_COMMAND
                 ${VPX_APPLE_LDFLAGS}
-                <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+                <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> ${VPX_TOOLCHAIN_FLAG}
                 --enable-shared --disable-static --disable-examples
             BUILD_COMMAND $(MAKE) -j${AV_BUILD_DEPENDENCIES_PARALLEL}
         )
